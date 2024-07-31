413f711ed12b58a9776f03f35e0cb758c0c55876
chenming.zhang <bigwater.zhang@gmail.com>
2024-07-31 11:09:29 +0800

    crestereo dataset

---
+++ cfgs/common/datasets/crestereo.py
@@ -0,0 +1,35 @@
+# @Time    : 2024/6/9 18:13
+# @Author  : zhangchenming
+import os
+from stereo.config.lazy import LazyCall
+from stereo.datasets.crestereo_dataset import CREStereoDataset
+from stereo.datasets.utils import stereo_trans
+from stereo.datasets import build_dataloader
+
+from cfgs.common.runtime_params import data_root_dir
+from cfgs.common.constants import constants
+
+data_root_path = os.path.join(data_root_dir, 'CREStereoData')
+
+train = LazyCall(CREStereoDataset)(
+    data_root_path=data_root_path,
+    split_file='./data/CREStereo/crestereo_train.txt',
+    augmentations=None,
+    return_right_disp=True
+)
+
+val = LazyCall(CREStereoDataset)(
+    data_root_path=data_root_path,
+    split_file='./data/CREStereo/crestereo_val.txt',
+    augmentations=[
+        LazyCall(stereo_trans.CropOrPad)(size=[800, 1760]),
+        LazyCall(stereo_trans.NormalizeImage)(mean=constants.imagenet_rgb_mean, std=constants.imagenet_rgb_std)
+    ]
+)
+
+val_loader = LazyCall(build_dataloader)(
+    all_dataset=[val],
+    batch_size=1,
+    workers=8,
+    pin_memory=True,
+    shuffle=False)
---
+++ stereo/datasets/crestereo_dataset.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+from PIL import Image
+from .dataset_template import DatasetTemplate
+import cv2
+
+
+class CREStereoDataset(DatasetTemplate):
+    def __init__(self, data_root_path, split_file, augmentations, return_right_disp):
+        super().__init__(data_root_path, split_file, augmentations)
+        self.return_right_disp = return_right_disp
+
+    def __getitem__(self, idx):
+        item = self.data_list[idx]
+        full_paths = [os.path.join(self.root, x) for x in item]
+        left_path, right_path, left_disp_path = full_paths
+
+        left_img = Image.open(left_path).convert('RGB')
+        left_img = np.array(left_img, dtype=np.float32)
+
+        right_img = Image.open(right_path).convert('RGB')
+        right_img = np.array(right_img, dtype=np.float32)
+
+        left_disp = cv2.imread(left_disp_path, cv2.IMREAD_UNCHANGED)
+        left_disp = left_disp.astype(np.float32) / 32
+
+        sample = {
+            'left': left_img,
+            'right': right_img,
+            'disp': left_disp
+        }
+
+        if self.return_right_disp:
+            right_disp_path = left_disp_path.replace('left', 'right')
+            right_disp = cv2.imread(right_disp_path, cv2.IMREAD_UNCHANGED)
+            right_disp = right_disp.astype(np.float32) / 32
+            sample['disp_right'] = right_disp
+
+        if self.augmentations is not None:
+            for t in self.augmentations:
+                sample = t(sample)
+
+        sample['index'] = idx
+        sample['name'] = left_path
+
+        return sample
+    
