bdd1c86362b2ae9b03928ee24f1669c1025f6841
chenming.zhang <bigwater.zhang@gmail.com>
2024-07-31 11:12:49 +0800

    instereo2k dataset

---
+++ cfgs/common/datasets/instereo2k.py
@@ -0,0 +1,35 @@
+# @Time    : 2024/6/9 18:13
+# @Author  : zhangchenming
+import os
+from stereo.config.lazy import LazyCall
+from stereo.datasets.instereo2k_dataset import InStereo2KDataset
+from stereo.datasets.utils import stereo_trans
+from stereo.datasets import build_dataloader
+
+from cfgs.common.runtime_params import data_root_dir
+from cfgs.common.constants import constants
+
+data_root_path = os.path.join(data_root_dir, 'InStereo2K')
+
+train = LazyCall(InStereo2KDataset)(
+    data_root_path=data_root_path,
+    split_file='./data/InStereo2K/InStereo2K_train.txt',
+    augmentations=None,
+    return_right_disp=True
+)
+
+val = LazyCall(InStereo2KDataset)(
+    data_root_path=data_root_path,
+    split_file='./data/InStereo2K/InStereo2K_test.txt',
+    augmentations=[
+        LazyCall(stereo_trans.CropOrPad)(size=[800, 1760]),
+        LazyCall(stereo_trans.NormalizeImage)(mean=constants.imagenet_rgb_mean, std=constants.imagenet_rgb_std)
+    ]
+)
+
+val_loader = LazyCall(build_dataloader)(
+    all_dataset=[val],
+    batch_size=1,
+    workers=8,
+    pin_memory=True,
+    shuffle=False)
