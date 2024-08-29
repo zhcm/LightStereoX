70494366479e84f3360deb147fba6ab5df336236
chenming.zhang <bigwater.zhang@gmail.com>
2024-08-30 02:21:04 +0800

    add argoverse dataset

---
+++ cfgs/common/datasets/argoverse.py
@@ -0,0 +1,34 @@
+# @Time    : 2024/6/9 18:13
+# @Author  : zhangchenming
+import os
+from stereo.config.lazy import LazyCall
+from stereo.datasets.argoverse_dataset import ArgoverseDataset
+from stereo.datasets.utils import stereo_trans
+from stereo.datasets import build_dataloader
+
+from cfgs.common.runtime_params import data_root_dir
+from cfgs.common.constants import constants
+
+data_root_path = os.path.join(data_root_dir, 'Argoverse')
+
+train = LazyCall(ArgoverseDataset)(
+    data_root_path=data_root_path,
+    split_file='./data/Argoverse/argoverse_train.txt',
+    augmentations=None
+)
+
+val = LazyCall(ArgoverseDataset)(
+    data_root_path=data_root_path,
+    split_file='./data/Argoverse/argoverse_val.txt',
+    augmentations=[
+        LazyCall(stereo_trans.DivisiblePad)(divisor=32, mode='round'),
+        LazyCall(stereo_trans.NormalizeImage)(mean=constants.imagenet_rgb_mean, std=constants.imagenet_rgb_std)
+    ]
+)
+
+val_loader = LazyCall(build_dataloader)(
+    all_dataset=[val],
+    batch_size=1,
+    workers=8,
+    pin_memory=True,
+    shuffle=False)
---
+++ stereo/datasets/argoverse_dataset.py
@@ -0,0 +1,38 @@
+# @Time    : 2024/8/29 17:00
+# @Author  : zhangchenming
+import os
+import numpy as np
+import cv2
+from .dataset_template import DatasetTemplate
+
+
+class ArgoverseDataset(DatasetTemplate):
+    def __init__(self, data_root_path, split_file, augmentations):
+        super().__init__(data_root_path, split_file, augmentations)
+
+    def __getitem__(self, idx):
+        item = self.data_list[idx]
+        full_paths = [os.path.join(self.root, x) for x in item]
+        left_path, right_path, disp_path = full_paths
+
+        left_img = cv2.cvtColor(cv2.imread(left_path), cv2.COLOR_BGR2RGB)
+
+        right_img = cv2.cvtColor(cv2.imread(right_path), cv2.COLOR_BGR2RGB)
+
+        disp_img = cv2.imread(disp_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+        disp_img = np.float32(disp_img) / 256.0
+
+        sample = {
+            'left': left_img,
+            'right': right_img,
+            'disp': disp_img,
+        }
+
+        if self.augmentations is not None:
+            for t in self.augmentations:
+                sample = t(sample)
+
+        sample['index'] = idx
+        sample['name'] = left_path
+
+        return sample
