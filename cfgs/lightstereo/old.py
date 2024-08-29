91a0e746bcb182234e7b4e42f954296f341f6338
chenming.zhang <bigwater.zhang@gmail.com>
2024-08-30 02:20:37 +0800

    add old lightstereo

---
+++ cfgs/lightstereo/old.py
@@ -0,0 +1,64 @@
+# @Time    : 2024/6/9 12:32
+# @Author  : zhangchenming
+import os
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import OneCycleLR
+
+from stereo.config.lazy import LazyCall, LazyConfig
+from stereo.datasets import build_dataloader
+from stereo.datasets.utils import stereo_trans
+from stereo.modeling.models.old_lightstereo.lightstereo import LightStereo
+from stereo.solver.build import get_model_params, ClipGradValue
+
+from cfgs.common.runtime_params import runtime_params, project_root_dir
+from cfgs.common.constants import constants
+
+# dataset
+train_augmentations = [
+    LazyCall(stereo_trans.RandomCrop)(crop_size=[320, 736]),
+    LazyCall(stereo_trans.NormalizeImage)(mean=constants.imagenet_rgb_mean, std=constants.imagenet_rgb_std)
+]
+
+sceneflow = LazyConfig.load('cfgs/common/datasets/sceneflow.py')
+sceneflow.train.augmentations = train_augmentations
+sceneflow.train.return_right_disp = False
+
+# dataloader
+batch_size_per_gpu = 3
+train_loader = LazyCall(build_dataloader)(
+    is_dist=None,
+    all_dataset=[sceneflow.train],
+    batch_size=batch_size_per_gpu,
+    shuffle=True,
+    workers=8,
+    pin_memory=True)
+
+val_loader = LazyCall(build_dataloader)(
+    is_dist=None,
+    all_dataset=[sceneflow.val],
+    batch_size=batch_size_per_gpu,
+    shuffle=False,
+    workers=8,
+    pin_memory=True)
+
+# model
+model = LazyCall(LightStereo)(aggregation_blocks=[8, 16, 32], expanse_ratio=8)
+
+# optim
+lr = 0.0001 * batch_size_per_gpu
+optimizer = LazyCall(AdamW)(
+    params=LazyCall(get_model_params)(model=None),
+    lr=lr,
+    weight_decay=1.0e-05,
+    eps=1.0e-08)
+
+# scheduler
+scheduler = LazyCall(OneCycleLR)(optimizer=None, max_lr=lr, total_steps=-1, pct_start=0.01, cycle_momentum=False, anneal_strategy='linear')
+
+# clip grad
+clip_grad = LazyCall(ClipGradValue)(clip_value=1.0)
+
+# runtime params
+runtime_params.save_root_dir = os.path.join(project_root_dir, 'output/SceneflowDataset/StereoBase')
+runtime_params.train_epochs = 90
+runtime_params.mixed_precision = True
