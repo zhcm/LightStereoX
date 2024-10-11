6403201113e8110d1dd417fae2f868f34e4d7a59
chenming.zhang <bigwater.zhang@gmail.com>
2024-10-11 13:05:57 +0800

    add tartanair

---
+++ cfgs/common/datasets/tartanair.py
@@ -0,0 +1,20 @@
+# @Time    : 2024/10/10 22:36
+# @Author  : zhangchenming
+import os
+from stereo.config.lazy import LazyCall
+from stereo.datasets.tartanair_dataset import TartanAirDataset
+from stereo.datasets.utils import stereo_trans
+from stereo.datasets import build_dataloader
+
+from cfgs.common.runtime_params import data_root_dir
+from cfgs.common.constants import constants
+
+data_root_path = os.path.join(data_root_dir, 'TartanAir')
+
+train = LazyCall(TartanAirDataset)(
+    data_root_path=data_root_path,
+    split_file='./data/TartanAir/all_306637.txt',
+    augmentations=None
+)
+
+# (480, 640)
