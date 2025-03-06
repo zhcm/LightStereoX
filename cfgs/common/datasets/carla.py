7dca05839f7be421e3a96c88a00b16c718033ca9
chenming.zhang <bigwater.zhang@gmail.com>
2025-03-07 03:56:56 +0800

    update

--- cfgs/common/datasets/carla.py
+++ cfgs/common/datasets/carla.py
@@ -3,11 +3,8 @@
 import os
 from stereo.config.lazy import LazyCall
 from stereo.datasets.carla_dataset import CarlaDataset
-from stereo.datasets.utils import stereo_trans
-from stereo.datasets import build_dataloader
 
 from cfgs.common.runtime_params import data_root_dir
-from cfgs.common.constants import constants
 
 data_root_path = os.path.join(data_root_dir, 'StereoFromCarla')
 
